b2779ea159b874d2809266f262b9be8460cd33a5
Krit <yothinpeace@gmail.com>
2023-01-18 16:41:52 +0700

    ENH: Blank pygame window

---
+++ code/interface.py
@@ -0,0 +1,25 @@
+# import pygame package
+import pygame
+  
+# initializing imported module
+pygame.init()
+  
+# displaying a window of height
+# 500 and width 400
+pygame.display.set_mode((750, 750))
+  
+# creating a bool value which checks
+# if game is running
+running = True
+  
+# keep game running till running is true
+while running:
+      
+    # Check for event if user has pushed
+    # any event in queue
+    for event in pygame.event.get():
+          
+        # if event is of type quit then 
+        # set running bool to false
+        if event.type == pygame.QUIT:
+            running = False
