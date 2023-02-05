fcb3495c58c008b27af98635232c144cb67107d3
Krit <yothinpeace@gmail.com>
2023-02-05 21:54:09 +0700

    ENH: Change window size & background color

--- code/app.py
+++ code/app.py
@@ -1,13 +1,24 @@
 import tkinter as tk
 
 from frame import MainFrame
+from color import Color
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
-        # configure the root window
-        self.title('My Awesome App')
-        self.geometry('300x100')
+        self.title('Base System')
+
+        # App window dimension
+        window_width = 900
+        window_height = 780
+
+        # Find the center point
+        center_x = int(self.winfo_screenwidth()/2 - window_width / 2)
+        center_y = 0
+
+        self.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+        self.resizable(False, False)
+        self.configure(bg=Color.darkgray)
 
 if __name__ == "__main__":
     app = App()
--- code/color.py
+++ code/color.py
@@ -2,3 +2,4 @@ class Color:
     red    = (255, 0, 0)
     green  = (0, 255, 0)
     blue   = (0, 0, 255)
+    darkgray    = "#3C4048"
