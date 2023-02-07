85e118a96ca032fc9abf8a6f4decf4b3a5eccda3
Krit <yothinpeace@gmail.com>
2023-02-07 21:47:20 +0700

    CLN: Tray and Navigation code cleanup

--- code/canvas.py
+++ code/canvas.py
@@ -35,8 +35,8 @@ class Canvas():
             column (int) : number of columns
             color (str)  : color code 
         """
-        self.grid_x = x
-        self.grid_y = y
+        self.pixel_grid_offset_x = x
+        self.pixel_grid_offset_y = y
         for r in range(row+1):
             self.canvas.create_line((x+560-8*r, y+280+120-4*r), (x+560+240-8*r, y+280-4*r), width=1, fill=color)
 
@@ -55,29 +55,67 @@ class Canvas():
         self.canvas.image = ImageTk.PhotoImage(image)
         self.canvas.create_image(x, y, image=self.canvas.image)
 
-    def map_3D_to_2D(self, x, y, z):
-        pixel_x =   8*(x+15) - 8*(y+35) + 560        + self.grid_x
-        pixel_y = -(4*(x+15) + 4*(y+35) - 400 + 8*z) + self.grid_y
-        print(pixel_x, pixel_y)
+    def map_3D_to_2D(self, grid_x, grid_y, grid_z):
+        pixel_x = self.pixel_grid_offset_x +  8*(grid_x+15) - 8*(grid_y+35) + 560
+        pixel_y = self.pixel_grid_offset_y - (4*(grid_x+15) + 4*(grid_y+35) - 400 + 8*grid_z)
         return pixel_x, pixel_y
 
-    def map_tray_points(self, x, y, w, h, theta):
+    def map_tray_points(self, grid_x, grid_y, tray_width, tray_height, tray_thick, orientation):
         """
         w = 6
         h = 5
         """
-        r = (w**2 + h**2) ** 0.5
-        theta = math.radians(theta)
-        tray_theta = math.atan(w/h)
 
-        point_1 = (x, y)
-        point_2 = (x + w*math.cos(theta), x - w*math.sin(theta))
-        point_3 = (r * math.sin(theta+tray_theta), r * math.cos(theta+tray_theta))
-        point_4 = (y + h*math.sin(theta), y + h*math.cos(theta))
+        tray_diagonal = (tray_width**2 + tray_height**2) ** 0.5
+        theta = math.radians(orientation)
+        tray_theta = math.atan(tray_width / tray_height)
 
-        print(point_1, point_2, point_3, point_4)
+        tray_points = {
+            "bottom_tray" : (
+                self.map_3D_to_2D( grid_x, grid_y, 0 ),
+                self.map_3D_to_2D( grid_x + tray_width    * math.cos(theta),            grid_y - tray_width    * math.sin(theta),            0 ),
+                self.map_3D_to_2D( grid_x + tray_diagonal * math.sin(theta+tray_theta), grid_y + tray_diagonal * math.cos(theta+tray_theta), 0 ),
+                self.map_3D_to_2D( grid_x + tray_height   * math.sin(theta),            grid_y + tray_height   * math.cos(theta),            0 ),
+            ),
+            "top_tray" : (
+                self.map_3D_to_2D( grid_x, grid_y, tray_thick ),
+                self.map_3D_to_2D( grid_x + tray_width    * math.cos(theta),            grid_y - tray_width    * math.sin(theta), tray_thick ),
+                self.map_3D_to_2D( grid_x + tray_diagonal * math.sin(theta+tray_theta), grid_y + tray_diagonal * math.cos(theta+tray_theta), tray_thick ),
+                self.map_3D_to_2D( grid_x + tray_height   * math.sin(theta),            grid_y + tray_height   * math.cos(theta), tray_thick )
+            ),
+        }
 
-        return (point_1, point_2, point_3, point_4)
+        if orientation <= 45:
+            left_side  = (3, 0)
+            right_side = (0, 1) 
+        elif orientation <= 135:
+            left_side  = (0, 1)
+            right_side = (1, 2) 
+        elif orientation <= 225:
+            left_side  = (1, 2)
+            right_side = (2, 3) 
+        elif orientation <= 315:
+            left_side  = (2, 3)
+            right_side = (3, 0) 
+        else:
+            left_side  = (3, 0)
+            right_side = (0, 1) 
+
+        tray_points["left_wall"] = ( 
+            tray_points["bottom_tray"][left_side[0]], 
+            tray_points["bottom_tray"][left_side[1]], 
+            tray_points["top_tray"][left_side[1]], 
+            tray_points["top_tray"][left_side[0]]
+        )
+        tray_points["right_wall"] = ( 
+            tray_points["bottom_tray"][right_side[0]], 
+            tray_points["bottom_tray"][right_side[1]], 
+            tray_points["top_tray"][right_side[1]], 
+            tray_points["top_tray"][right_side[0]]
+        )
+
+        return tray_points
+
 
     def create_tray(self, origin_x, origin_y, orientation):
         """
@@ -89,47 +127,51 @@ class Canvas():
             orientation (float) : tray orientation in degree
         """
 
-        #Bottom tray
-        points = self.map_tray_points(origin_x, origin_y, 6, 5, orientation)
-        pixels = (
-            self.map_3D_to_2D(points[0][0], points[0][1], 0),
-            self.map_3D_to_2D(points[1][0], points[1][1], 0),
-            self.map_3D_to_2D(points[2][0], points[2][1], 0),
-            self.map_3D_to_2D(points[3][0], points[3][1], 0),
-        )
-        self.canvas.create_polygon(*pixels, fill=Color.gray, outline="")
+        tray_points = self.map_tray_points(origin_x, origin_y, 6, 5, 0.5, orientation)
+        self.canvas.create_polygon(*tray_points["bottom_tray"], fill=Color.gray, outline="")
+        self.canvas.create_polygon(*tray_points["top_tray"],    fill=Color.lightgray, outline="")
+        self.canvas.create_polygon(*tray_points["left_wall"],   fill=Color.middlegray, outline="")
+        self.canvas.create_polygon(*tray_points["right_wall"],  fill=Color.gray, outline="")
 
-    def create_navigator(self, x, y, z):
+    def map_navigator_points(self, grid_x, grid_y, grid_z):
+        pixel_x, pixel_y = self.map_3D_to_2D(grid_x, grid_y, grid_z)
 
-        x,y = self.map_3D_to_2D(x, y, z)
+        navigator_points = {
+            "navigator_tip" : (pixel_x, pixel_y),
+            "navigator_top" : (
+                (pixel_x,      pixel_y - 16),
+                (pixel_x - 16, pixel_y - 24),
+                (pixel_x,      pixel_y - 32),
+                (pixel_x + 16, pixel_y - 24),
+            ),
+            "navigator_left" : (
+                (pixel_x,      pixel_y),
+                (pixel_x - 16, pixel_y - 24),
+                (pixel_x,      pixel_y - 16),
+            ),
+            "navigator_right" : (
+                (pixel_x,      pixel_y),
+                (pixel_x + 16, pixel_y - 24),
+                (pixel_x,      pixel_y - 16),
+            )
+        }
+
+        return navigator_points
+
+    def create_navigator(self, grid_x, grid_y, grid_z):
+
+        navigator_points = self.map_navigator_points(grid_x, grid_y, grid_z)
 
         self.laser_on = True
         if self.laser_on:
-            self.canvas.create_line((x, y-5), (x, y+z*8), width=2, fill=Color.red)
+            pixel_x = navigator_points["navigator_tip"][0]
+            pixel_y = navigator_points["navigator_tip"][1]
+            points = (
+                (pixel_x, pixel_y - 5),
+                (pixel_x, pixel_y + grid_z*8),
+            )
+            self.canvas.create_line(*points, width=2, fill=Color.red)
 
-        points = (
-            (x,    y-16),
-            (x-16, y-24),
-            (x,    y-32),
-            (x+16, y-24),
-        )
-        self.canvas.create_polygon(*points, fill="#FFD18C", outline="")
-
-        points = (
-            (x,    y),
-            (x-16, y-24),
-            (x,    y-16),
-        )
-        self.canvas.create_polygon(*points, fill="#FFB545", outline="")
-
-        points = (
-            (x,    y),
-            (x+16, y-24),
-            (x,    y-16),
-        )
-        self.canvas.create_polygon(*points, fill="#EAA031", outline="")
-
-
-
-
-    
+        self.canvas.create_polygon(*navigator_points["navigator_top"], fill="#FFD18C", outline="")
+        self.canvas.create_polygon(*navigator_points["navigator_left"], fill="#FFB545", outline="")
+        self.canvas.create_polygon(*navigator_points["navigator_right"], fill="#EAA031", outline="")
--- code/color.py
+++ code/color.py
@@ -2,6 +2,7 @@ class Color:
     white       = "#FFFFFF"
     whitegray   = "#EAEAEA"
     lightgray   = "#B2B2B2"
+    middlegray  = "#A4A4A4"
     gray        = "#8C8C8C"
     darkgray    = "#3C4048"
     lightblue   = "#0AD1DA"
