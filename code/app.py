afdae904da2646343eb0e72c9255e6fa1f574c1b
Krit <yothinpeace@gmail.com>
2023-03-28 21:31:34 +0700

    ENH: Radio Button with Deletion

--- code/app.py
+++ code/app.py
@@ -8,7 +8,7 @@ from components.tray import Tray
 from components.target import Target
 from components.navigator import Navigator
 
-from components.button import ToggleButton
+from components.button import ToggleButton, RadioButton
 
 from function import *
 from keyboard import Keyboard
@@ -16,36 +16,68 @@ from keyboard import Keyboard
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+        #Title
         self.title('Base System')
-
         #Mode
         self.mode = "Developer"
-
-        # App window dimension
+        #Window Dimension
         window_width = 900
         window_height = 780
-
-        # Find the center point
+        #Find Center Point
         center_x = int(self.winfo_screenwidth()/2 - window_width / 2)
         center_y = 0
-
+        #Set Window Properties
         self.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
         self.resizable(False, False)
         self.configure(bg=Color.darkgray)
-
-        # Add font
+        #Add font
         pyglet.font.add_file('font/Inter-Bold.ttf')
         pyglet.font.add_file('font/Inter-SemiBold.ttf')
+        #Components
+        self.create_components()
 
-        self.grid_position = (0, 0)
+    def create_components(self):
+        self.field_canvas = Canvas(self, 840, 540, 30)
+        self.field_canvas.create_round_rectangle(600, 26, 240+20, 80, 20, Color.darkgray)
+        self.field_canvas.create_textbox(690, 52, "Module III", 26, Color.lightblue)
+        self.field_canvas.create_textbox(690, 82, "Base System", 19, Color.whitegray)
+        self.field_canvas.create_photo("logo", 800, 66)
+
+        self.grid = Grid(self.field_canvas, 20, 120, 70, 30, Color.lightgray)
+        self.pick_tray = Tray(root_canvas=self.field_canvas, root_grid=self.grid) 
+        self.target =  Target(root_canvas=self.field_canvas, root_grid=self.grid, grid_x=10, grid_y=10)
+        self.navi = Navigator(root_canvas=self.field_canvas, root_grid=self.grid, grid_x=-5, grid_y=10, grid_z=8)
+        
+        self.command_canvas = Canvas(self, 840, 150, 0)
+        self.command_canvas.create_textbox(75, 25, "End Effector", 16, Color.darkgray)
+        self.command_canvas.create_textbox(75, 55, "Laser", 13, Color.darkgray)
+        self.command_canvas.create_textbox(75, 75, "Gripper", 13, Color.darkgray)
+
+        self.command_canvas.create_textbox(400, 25, "Operation", 16, Color.darkgray)
+        self.command_canvas.create_textbox(700, 25, "Movement", 16, Color.darkgray)
+        # command_canvas.create_rectangle_button(0, 0, 100, 50, 20, Color.blue, "Run", 20, Color.white, hello)
+        # command_canvas.create_radio_button(100, 100, 14, Color.blue, "active", hello)
+        # command_canvas.create_radio_button(200, 100, 14, Color.blue, "inactive", hello)
+        
+        # toggle = ToggleButton(root_canvas=self.command_canvas, x=300, y=100, w=36, h=20, active_color=Color.blue, active_text="On", inactive_color=Color.lightgray, inactive_text="Off", text_size=12, function=laser_transmit)
+        self.radio_1 = RadioButton(root_canvas=self.command_canvas, x=100, y=100, r=14, active_color=Color.blue, active_text="On", inactive_color=Color.lightgray, inactive_text="Off", text_size=12)
+
+        if self.mode == "Developer":
+            keyboard = Keyboard(self)
+            keyboard.key_bind(self)
+
+        # app.bind("<ButtonRelease-1>", mouse_position)
+        # app.bind("<Motion>", mouse_position)
+        # field_canvas.canvas.bind("<Motion>", mouse_position)
+        self.field_canvas.canvas.bind("<ButtonRelease-1>", mouse_position)
+
 
     def task(self):
+        #Remove in the Future
         self.pick_tray.clear_tray()
         self.pick_tray.create_tray()
-
         self.target.clear_target()
         self.target.create_target()
-        #Get navi position then redraw
         self.navi.clear_navigator()
         self.navi.create_navigator()
 
@@ -53,33 +85,5 @@ class App(tk.Tk):
 
 if __name__ == "__main__":
     app = App()
-
-    field_canvas = Canvas(app, 840, 540, 30)
-    field_canvas.create_round_rectangle(600, 26, 240+20, 80, 20, Color.darkgray)
-    field_canvas.create_textbox(690, 52, "Module III", 26, Color.lightblue)
-    field_canvas.create_textbox(690, 82, "Base System", 19, Color.whitegray)
-    field_canvas.create_photo("logo", 800, 66)
-
-    grid = Grid(field_canvas, 20, 120, 70, 30, Color.lightgray)
-    app.pick_tray = Tray(root_canvas=field_canvas, root_grid=grid) 
-    app.target =  Target(root_canvas=field_canvas, root_grid=grid, grid_x=10, grid_y=10)
-    app.navi = Navigator(root_canvas=field_canvas, root_grid=grid, grid_x=-5, grid_y=10, grid_z=8)
-    
-    command_canvas = Canvas(app, 840, 150, 0)
-    # command_canvas.create_rectangle_button(0, 0, 100, 50, 20, Color.blue, "Run", 20, Color.white, hello)
-    # command_canvas.create_radio_button(100, 100, 14, Color.blue, "active", hello)
-    # command_canvas.create_radio_button(200, 100, 14, Color.blue, "inactive", hello)
-    
-    toggle = ToggleButton(root_canvas=command_canvas, x=300, y=100, w=36, h=20, active_color=Color.blue, active_text="On", inactive_color=Color.lightgray, inactive_text="Off", text_size=12, function=laser_transmit)
-    
-    if app.mode == "Developer":
-        keyboard = Keyboard(app)
-        keyboard.key_bind(app)
-
-    # app.bind("<ButtonRelease-1>", mouse_position)
-    # app.bind("<Motion>", mouse_position)
-    # field_canvas.canvas.bind("<Motion>", mouse_position)
-    field_canvas.canvas.bind("<ButtonRelease-1>", mouse_position)
-
     app.task()
     app.mainloop()
--- code/components/button.py
+++ code/components/button.py
@@ -29,15 +29,6 @@ class ToggleButton(Button):
         self.status = "inactive"
         self.create_toggle_button()
 
-    def clicked(self, event):
-        if self.status == "inactive":
-            self.status = "active"
-        else:
-            self.status = "inactive"
-        self.function(self.status)
-        # self.root_canvas.canvas.delete("all")
-        self.create_toggle_button()
-
     def create_toggle_button(self):
         x, y, w, h = self.x, self.y, self.w, self.h
         if self.status == "active":
@@ -48,3 +39,71 @@ class ToggleButton(Button):
             self.root_canvas.canvas.create_oval(x+3, y+3, x+h-3, y+h-3, fill=Color.white, outline="")  
         self.click_area = self.create_click_area(x, y, w, h, "rectangle")
         self.root_canvas.canvas.tag_bind(self.click_area, "<ButtonRelease-1>", self.clicked)
+
+    def clicked(self, event):
+        if self.status == "inactive":
+            self.status = "active"
+        else:
+            self.status = "inactive"
+        self.function(self.status)
+        # self.root_canvas.canvas.delete("all")
+        self.create_toggle_button()
+
+class RadioButton(Button):
+    """
+    RadioButton class
+    """
+    def __init__(self, root_canvas, x, y, r, active_color, active_text, inactive_color, inactive_text, text_size):
+        self.x = x
+        self.y = y
+        self.r = r
+        self.root_canvas = root_canvas
+        self.active_color = active_color
+        self.active_text  = active_text
+        self.inactive_color = inactive_color
+        self.inactive_text  = inactive_text
+        self.text_size = text_size
+        self.active = False
+        # self.function = function
+        # self.status = "inactive"
+        self.create_radio_button()
+
+    def create_radio_button(self):
+        x, y, r = self.x, self.y, self.r
+        if self.active == True:
+            self.outer_oval = self.root_canvas.canvas.create_oval(x, y, x+r, y+r, fill="", outline=self.active_color, width=2)
+            self.inner_oval = self.root_canvas.canvas.create_oval(x+4, y+4, x+r-4, y+r-4, fill=self.active_color, outline="")
+        elif self.active == False:
+            self.outer_oval = self.root_canvas.canvas.create_oval(x, y, x+r, y+r, fill="", outline=self.inactive_color, width=2)
+        self.click_area = self.create_click_area(x, y, r, r, "oval")
+        self.root_canvas.canvas.tag_bind(self.click_area, "<ButtonRelease-1>", self.clicked)
+
+    def delete_radio_button(self):
+        self.root_canvas.canvas.delete(self.outer_oval)
+        if not self.active:
+            self.root_canvas.canvas.delete(self.inner_oval)
+
+    def clicked(self, event):
+        if self.active == False:
+            self.active = True
+
+        else:
+            self.active = False
+
+        print(self.active)
+
+        self.delete_radio_button()
+        self.create_radio_button()
+
+
+
+    # def create_radio_button(self):
+    #     x, y, w, h = self.x, self.y, self.w, self.h
+    #     if self.status == "active":
+    #         self.root_canvas.create_round_rectangle(x, y, w, h, h/2, self.active_color)
+    #         self.root_canvas.canvas.create_oval(x+w-h+3, y+3, x+w-3, y+h-3, fill=Color.white, outline="")
+    #     elif self.status == "inactive":
+    #         self.root_canvas.create_round_rectangle(x, y, w, h, h/2, self.inactive_color)
+    #         self.root_canvas.canvas.create_oval(x+3, y+3, x+h-3, y+h-3, fill=Color.white, outline="")  
+    #     self.click_area = self.create_click_area(x, y, w, h, "rectangle")
+    #     self.root_canvas.canvas.tag_bind(self.click_area, "<ButtonRelease-1>", self.clicked)
