2ff1b65bfafa377d03a2fbef1df1216ed7dd5959
Krit <yothinpeace@gmail.com>
2023-04-11 13:53:33 +0700

    CLN: Remove old way of moving

--- code/keyboard.py
+++ code/keyboard.py
@@ -6,30 +6,17 @@ class Keyboard():
         self.app = app
         
     def key_left(self, event):
-        # self.app.navi.grid_x   -= 0.1
-        # self.app.target.grid_x -= 0.1
         self.app.navi.move_to(self.app.navi.grid_x-0.1, self.app.navi.grid_y)
         self.app.target.move_to(self.app.target.grid_x-0.1, self.app.target.grid_y)
     def key_right(self, event):
-        # self.app.navi.grid_x   += 0.1
-        # self.app.target.grid_x += 0.1
         self.app.navi.move_to(self.app.navi.grid_x+0.1, self.app.navi.grid_y)
         self.app.target.move_to(self.app.target.grid_x+0.1, self.app.target.grid_y)
     def key_up(self, event):
-        # self.app.navi.grid_y   += 0.1
-        # self.app.target.grid_y += 0.1
         self.app.navi.move_to(self.app.navi.grid_x, self.app.navi.grid_y+0.1)
         self.app.target.move_to(self.app.target.grid_x, self.app.target.grid_y+0.1)
     def key_down(self, event):
-        # self.app.navi.grid_y   -= 0.1
-        # self.app.target.grid_y -= 0.1
         self.app.navi.move_to(self.app.navi.grid_x, self.app.navi.grid_y-0.1)
         self.app.target.move_to(self.app.target.grid_x, self.app.target.grid_y-0.1)
-    # def key_k(self, event):
-    #     self.app.navi.laser_on = False
-    # def key_l(self, event):
-    #     self.app.navi.laser_on = True
-
     def key_a(self, event):
         self.app.tray_pick.origin_x -= 0.1
     def key_d(self, event):
@@ -48,8 +35,6 @@ class Keyboard():
         app.bind("<KeyPress-Right>", self.key_right)
         app.bind("<KeyPress-Up>", self.key_up)
         app.bind("<KeyPress-Down>", self.key_down)
-        # app.bind("<KeyPress-k>", self.key_k)
-        # app.bind("<KeyPress-l>", self.key_l)
         app.bind("<KeyPress-a>", self.key_a)
         app.bind("<KeyPress-d>", self.key_d)
         app.bind("<KeyPress-w>", self.key_w)
